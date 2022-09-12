3fc38f38b83787c2cf0abef4de96edde897a47f3
200402 <r_he_lol@mail.ru>
2022-09-13 04:17:39 +0500

    Добавьте файлы проекта.

---
+++ BridgePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем нового программиста, он работает с с++
+            Programmer freelancer = new FreelanceProgrammer(new CPPLanguage());
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+            // пришел новый заказ, но теперь нужен c#
+            freelancer.Language = new CSharpLanguage();
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+
+            Console.Read();
+        }
+    }
+}
